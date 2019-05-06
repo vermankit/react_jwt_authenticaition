0a98b4966b2659cfa0be982d9e337cf45f5cfdfe
Ankit Verma <ankit@tomhorngaming.com>
2019-05-06 15:09:06 +0530

    Added CSS

--- react_jwt_sample/src/components/Login.jsx
+++ react_jwt_sample/src/components/Login.jsx
@@ -1,27 +1,30 @@
-import React ,{useState} from 'react';
+import React, { useState } from 'react';
 
 
 const Login = (props) => {
-    const [username,setUsername] = useState("");
-    const [password,setPassword] = useState("");
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
 
-    let HandleSubmit = function(e){
-         e.preventDefault();
-         console.log(username + ' ' +password);
+    let HandleSubmit = function (e) {
+        e.preventDefault();
+        console.log(username + ' ' + password);
     }
 
-    return <form>
-                <h1>Login Page</h1>
-                <div>
-                    <label>Username</label>
-                    <input type='text' onChange={({target}) => setUsername(target.value)}></input>
-                </div>
-                <div>
-                    <label>Password</label>
-                    <input type='password' onChange={({target}) => setPassword(target.value)}></input>
-                </div>
-                <button type="submit" onClick={HandleSubmit}>sign in </button>
-            </form>
+    return    <div className="container">
+                <form onSubmit={HandleSubmit}>
+                    <h1>Login Page</h1>
+                    <div className="text">
+                        <label>Username</label>
+                        <input type='text' onChange={({ target }) => setUsername(target.value)}></input>
+                    </div>
+                    <div className="">
+                        <label>Password</label>
+                        <input type='password' onChange={({ target }) => setPassword(target.value)}></input>
+                    </div>
+                    <button className="button"type="submit" >sign in </button>
+                </form>
+            </div>
+
 }
 
 export default Login;
