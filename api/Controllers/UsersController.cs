9ce2d7450a2d2a9c4fb30fa0e366811bd5caa902
Ankit Verma <ankit@tomhorngaming.com>
2019-06-06 14:40:30 +0530

    Created api for backend

---
+++ api/Controllers/UsersController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+namespace api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class UsersController : ControllerBase
+    {
+        // GET api/values
+        [AllowAnonymous]
+        [HttpPost("authenticate")]                
+        public IActionResult Authenticate()
+        {
+            return null;
+        }       
+    }
+}
--- react_jwt_sample/src/services/authenticate.service.js
+++ react_jwt_sample/src/services/authenticate.service.js
@@ -1,10 +1,20 @@
+import axios from "axios";
 
 
-
+// Creating an object using literals
+//https://www.phpied.com/3-ways-to-define-a-javascript-class/
 export const AuthenticateService = {
     authenticate
 };
 
- function authenticate(user, pass) {
-
+function authenticate(user, pass) {
+    let data = {
+        username: user,
+        password: pass
+    };
+    let url = `${process.env.REACT_APP_API}`;
+    axios.post(`${url}/authenticate`,data)
+    .then(function(response){
+       console.log(response)
+    });
 }
